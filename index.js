bdbea3ee546192168e7dab1c1c8a7697d51c21d9
karyeun <karyeun@yahoo.com>
2018-03-21 23:44:11 +0800

    complete structure w/ argv - node index MMP 10 true

--- index.js
+++ index.js
@@ -1,42 +1,64 @@
-const sql = require('mssql');
-console.log('fun-mssql-mongo-migration');
+var sql = require('mssql');
+var db = require('./lib/db');
+var nconf = require('nconf');
+nconf.file('./config.json');
+var mssql_user = nconf.get('mssql-user');
+var mssql_password = nconf.get('mssql-password');
+var mssql_server = nconf.get('mssql-server');
+var mssql_database = nconf.get('mssql-database');
 
+console.log('fun-mssql-mongo-migration');
 console.log('args -> ');
 var gateway = '';
-var count = '*';
+var count = '0';
 var clean = false;
 process.argv.forEach(function(val, index, array) {
-    if (index > 1) console.log(index + ': ' + val);
+    //if (index > 1) console.log(index + ': ' + val);
     if (index === 2) gateway = val;
     else if (index === 3) count = val;
     else if (index === 4) clean = val;
 });
+
+console.log('gateway: ' + gateway);
+console.log('record to call: ' + (count == 0 ? '*' : count));
+console.log('clean mongodb: ' + clean);
+if (gateway == '') {
+    console.log('no gateway specified.');
+    process.exit(0);
+}
 
 if ('ICE,MEXCOMM,MK,MMP,'.indexOf(gateway + ',') < 0) {
     console.log('no gateway specified.');
     process.exit(0);
 }
 
+process.exit(0);
+
 //Data Source=sit-engagement-db.database.windows.net;
 //Initial Catalog=sit.boldlink;Persist Security Info=True;
 //User ID=sitengageadmeen;Password=Atos@2005
-const config = {
-    user: 'sitengageadmeen',
-    password: 'Atos@2005',
-    server: 'sit-engagement-db.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-    database: 'sit.boldlink',
+/******************************************/
+//"Server=WIN-0S1CKRDEL55\SQL2012;
+//Database=SMS_TransactionReporting;
+//User ID=sa;
+//Password=Funnet123
+var config = {
+    user: mssql_user,
+    password: mssql_password,
+    server: mssql_server,
+    database: mssql_database
 
-    options: {
-        encrypt: true // Use this if you're on Windows Azure
-    }
-}
+    // options: {
+    //     encrypt: true // Use this if you're on Windows Azure
+    // }
+};
 
 var table = 'tblSubscriber';
 if (gateway != 'MMP') {
     if (gateway == 'MEXCOMM') table += '_Mexcomm';
     else table += ('_' + gateway);
 }
-var query = 'select ' + (count != '*' ? 'top ' + count + ' ' : '') + '* from ' + table;
+var query = 'select ' + (count != '0' ? 'top ' + count + ' ' : '') + '* from ' + table;
 console.log(query);
 sql.connect(config).then(pool => {
     return pool.request()
@@ -68,7 +90,7 @@ sql.connect(config).then(pool => {
                 ' migrated.');
             counter++;
             if (counter == count) {
-                cosnole.log('migration of ' + count + ' subscriber(' + gateway + ') are done.')
+                cosnole.log('migration of ' + count + ' subscriber(' + gateway + ') are done.');
                 process.exit(0);
             }
         });
@@ -81,6 +103,4 @@ sql.connect(config).then(pool => {
 sql.on('error', err => {
     console.log(err);
     process.exit(1);
-})
-
-//process.exit(0);
+});
